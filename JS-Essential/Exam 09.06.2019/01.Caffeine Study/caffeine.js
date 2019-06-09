b54221d9a02db2144eebaa03399b7488e4a47a80
Todor Kamenov <todor8009@gmail.com>
2019-06-09 16:37:19 +0300

    Exam 09.06.2019

--- JS-Essential/Exam 09.06.2019/01.Caffeine Study/caffeine.js
+++ JS-Essential/Exam 09.06.2019/01.Caffeine Study/caffeine.js
@@ -20,25 +20,3 @@ let mornningCupOfDrinks = 3 * (1.5 * 40), lunchBottleCocaCola = 2 * (2.5 * 8), b
 solve(5);
 solve(8);
 
-// let morningCupOfCoffee = 1.5 * 40;
-// let lunchCocaCola = 2.5 * 8;
-// let brunchTeaCups = 3.5 * 20;
-// let totalFiveDaysEnergyDrinks = 5 * 30;
-//
-//
-// let morningCaffeine = 3 * morningCupOfCoffee;
-// let lunchCaffeine = 2 * lunchCocaCola;
-// let brunchCaffeine = 3 * brunchTeaCups;
-// let dayCaffeine = morningCaffeine + lunchCaffeine + brunchCaffeine;
-//
-// let result = 0;
-//
-// for (let i = 1; i <= Number(input); i++) {
-//     result += dayCaffeine;
-//
-//     if (i % 5 === 0) {
-//         result += 3 * totalFiveDaysEnergyDrinks;
-//     } else if (i % 9 === 0) {
-//         result += 4 * lunchCocaCola + 2 * totalFiveDaysEnergyDrinks;
-//     }
-// }
