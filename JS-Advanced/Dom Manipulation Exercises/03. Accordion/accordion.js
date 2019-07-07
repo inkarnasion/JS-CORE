f9ff732f2fb78cee59ad38f80aa705b55a32c4bd
Todor Kamenov <todor8009@gmail.com>
2019-07-07 15:44:10 +0300

    Finish task 3

--- JS-Advanced/Dom Manipulation Exercises/03. Accordion/accordion.js
+++ JS-Advanced/Dom Manipulation Exercises/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.querySelector('.button');
+    let extraDiv = document.getElementById('extra');
+
+    switch (button.textContent) {
+        case 'More':
+            button.textContent = 'Less';
+            extraDiv.style.display = 'block';
+            break;
+        default:
+            button.textContent = 'More';
+            extraDiv.style.display = 'none';
+            break;
+    }
 }
